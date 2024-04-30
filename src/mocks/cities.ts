bbab6cb427e6ece404361a3c051af6b5346c0c6f
IvanPetrushin <ivan24891@mail.ru>
2024-04-30 15:39:51 +0300

    Истина где-то на сервере

--- src/mocks/cities.ts
+++ src/mocks/cities.ts
@@ -1,40 +1,52 @@
-import { City } from '../types/city';
+// import { City } from '../types/city';
 
-export const cities: City[] = [
-  {
-    name: 'Paris',
-    latitude: 48.8534,
-    longitude: 2.3488,
-    zoom: 11
-  },
-  {
-    name: 'Amsterdam',
-    latitude: 52.37022,
-    longitude: 4.89517,
-    zoom: 11
-  },
-  {
-    name: 'Cologne',
-    latitude: 50.9333,
-    longitude: 6.95,
-    zoom: 11
-  },
-  {
-    name: 'Brussels',
-    latitude: 50.85034,
-    longitude: 4.35171,
-    zoom: 11
-  },
-  {
-    name: 'Hamburg',
-    latitude: 53.5753,
-    longitude: 10.015,
-    zoom: 11
-  },
-  {
-    name: 'Dusseldorf',
-    latitude: 51.2217,
-    longitude: 6.77616,
-    zoom: 11
-  }
-];
+// export const cities: City[] = [
+//   {
+//     name: 'Paris',
+//     location: {
+//       latitude: 48.8534,
+//       longitude: 2.3488,
+//       zoom: 11
+//     }
+//   },
+//   {
+//     name: 'Amsterdam',
+//     location: {
+//       latitude: 52.37022,
+//       longitude: 4.89517,
+//       zoom: 11
+//     }
+//   },
+//   {
+//     name: 'Cologne',
+//     location: {
+//       latitude: 50.9333,
+//       longitude: 6.95,
+//       zoom: 11
+//     }
+//   },
+//   {
+//     name: 'Brussels',
+//     location: {
+//       latitude: 50.85034,
+//       longitude: 4.35171,
+//       zoom: 11
+//     }
+//   },
+//   {
+//     name: 'Hamburg',
+//     location: {
+//       latitude: 53.5753,
+//       longitude: 10.015,
+//       zoom: 11
+//     }
+//   },
+//   {
+//     name: 'Dusseldorf',
+//     location: {
+//       latitude: 51.2217,
+//       longitude: 6.77616,
+//       zoom: 11
+//     }
+//   }
+// ];
