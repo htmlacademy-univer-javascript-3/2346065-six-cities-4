be584be7c1390f7ccee5b86ceb8c99d4186575c2
IvanPetrushin <ivan24891@mail.ru>
2024-05-10 23:07:18 +0300

    Добро пожаловать, или посторонним вход воспрещён (часть 1)0

--- src/mocks/points.ts
+++ src/mocks/points.ts
@@ -1,24 +1,24 @@
-import {Point} from '../types/point';
+// import {Point} from '../types/point';
 
-export const points: Point[] = [
-  {
-    latitude: 52.3909553943508,
-    longitude: 4.85309666406198,
-    zoom: 10
-  },
-  {
-    latitude: 48.8534,
-    longitude: 2.3488,
-    zoom: 10
-  },
-  {
-    latitude: 48.8334,
-    longitude: 2.3498,
-    zoom: 10
-  },
-  {
-    latitude: 51.2217,
-    longitude: 6.77616,
-    zoom: 10
-  },
-];
+// export const points: Point[] = [
+//   {
+//     latitude: 52.3909553943508,
+//     longitude: 4.85309666406198,
+//     zoom: 10
+//   },
+//   {
+//     latitude: 48.8534,
+//     longitude: 2.3488,
+//     zoom: 10
+//   },
+//   {
+//     latitude: 48.8334,
+//     longitude: 2.3498,
+//     zoom: 10
+//   },
+//   {
+//     latitude: 51.2217,
+//     longitude: 6.77616,
+//     zoom: 10
+//   },
+// ];
